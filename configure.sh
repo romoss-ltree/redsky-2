111b53939d03850969f7390dc9eb37b6e31659d7
romoss-ltree <92013025+romoss-ltree@users.noreply.github.com>
2022-09-13 02:11:55 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -9,14 +9,14 @@ cat << EOF > /etc/v2ray/config.json
         "protocol": "vmess",
         "settings": {
             "clients": [{
-                "id": "2112b632-fb6c-4781-bb42-d4d310aa532f",
+                "id": "825cdcd4-7073-474f-9f56-40e96f6a88b2",
                 "alterId": 0
             }]
         },
         "streamSettings": {
             "network": "ws",
             "wsSettings": {
-                "path": "/laowang"
+                "path": "/kam5lffha4we"
             }
         }
     }],
